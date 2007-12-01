18005e8b08945c821bd83220ff217213d3b47ecc
mikebotts <mike.botts@uah.edu>
2007-12-01 02:33:50 +0000

    some cleanup plus additional methods
    git-svn-id: http://sensorml-data-processing.googlecode.com/svn/common@49 fe31d19f-0751-0410-af65-b179d9baafec

--- src/org/vast/sensorML/SMLSerializer.java
+++ src/org/vast/sensorML/SMLSerializer.java
@@ -1,15 +1,43 @@
+/***************************** BEGIN LICENSE BLOCK ***************************
+
+ The contents of this file are subject to the Mozilla Public License Version
+ 1.1 (the "License"); you may not use this file except in compliance with
+ the License. You may obtain a copy of the License at
+ http://www.mozilla.org/MPL/MPL-1.1.html
+ 
+ Software distributed under the License is distributed on an "AS IS" basis,
+ WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ for the specific language governing rights and limitations under the License.
+ 
+ The Original Code is the "SensorML DataProcessing Engine".
+ 
+ The Initial Developer of the Original Code is the VAST team at the
+ University of Alabama in Huntsville (UAH). <http://vast.uah.edu>
+ Portions created by the Initial Developer are Copyright (C) 2007
+ the Initial Developer. All Rights Reserved.
+
+ Please Contact Mike Botts <mike.botts@uah.edu> for more information.
+ 
+ Contributor(s): 
+    Mike Botts <mike.botts@uah.edu>
+    Amanda Wyatt <awyatt2nsstc.uah.edu>
+ 
+******************************* END LICENSE BLOCK ***************************/
+
 package org.vast.sensorML;
 
-import org.vast.math.Vector3d;
+
 import org.vast.ogc.OGCRegistry;
 import org.vast.ows.SweResponseSerializer;
 import org.vast.xml.DOMHelper;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+import org.vast.math.Vector3d;
 
 public class SMLSerializer extends SweResponseSerializer {
 	
-	Element smlElt;
+	Element smlElt, systemElt;
+	public enum smlObjectType {COMPONENT,SYSTEM, PROCESS_MODEL,PROCESS_CHAIN};
 	
 	public SMLSerializer() {
 		
@@ -18,10 +46,16 @@ public class SMLSerializer extends SweResponseSerializer {
 	public void setTemplate(DOMHelper dom){
         super.setTemplate(dom);        
         dom.addUserPrefix("gml", OGCRegistry.getNamespaceURI(OGCRegistry.GML));
-        dom.addUserPrefix("om", OGCRegistry.getNamespaceURI(OGCRegistry.OM, "0.0"));
-        dom.addUserPrefix("swe", OGCRegistry.getNamespaceURI(OGCRegistry.SWE, "1.0"));
-        NodeList elts = dom.getDocument().getElementsByTagNameNS("http://www.opengis.net/om/0.0", "Observation");
+        //dom.addUserPrefix("om", OGCRegistry.getNamespaceURI(OGCRegistry.OM, "0.0"));
+        dom.addUserPrefix("swe", OGCRegistry.getNamespaceURI(OGCRegistry.SWE, "1.0.1"));
+        dom.addUserPrefix("sml", OGCRegistry.getNamespaceURI(OGCRegistry.SML, "1.0.1"));
+        dom.addUserPrefix("xlink", OGCRegistry.getNamespaceURI(OGCRegistry.XLINK, "1.0"));
+        NodeList elts = dom.getDocument().getElementsByTagNameNS("http://www.opengis.net/sml/1.0.1", "SensorML");
         smlElt = (Element)elts.item(0);
+        
+        //TODO: Check SensorML type of member (e.g. System, Component, etc.) 
+        //  and set the ENUM sensorMLObjectType; right now just use System
+        systemElt = dom.getElement(smlElt, "sml:SensorML/sml:member/sml:System");
 
 	}
 	   
@@ -49,11 +83,11 @@ public class SMLSerializer extends SweResponseSerializer {
     }
 */
 
-    
+	// TODO: need to find a way to make sure these are put into the right location
+	// of the SensorML instance
     public void setID(String id){
         try
         {
-           Element systemElt = dom.getElement(smlElt, "sml:SensorML/sml:member/sml:System");
            dom.setAttributeValue(systemElt, "@gml:id", id);        
          }
         catch (Exception e)
@@ -63,10 +97,11 @@ public class SMLSerializer extends SweResponseSerializer {
    	
     }
 	
+	// TODO: need to find a way to make sure these are put into the right location
+	// of the SensorML instance
 	public void setName(String name){
         try
         {
-           Element systemElt = dom.getElement(smlElt, "sml:SensorML/sml:member/sml:System");
            dom.setElementValue(systemElt, "gml:name", name);        
          }
         catch (Exception e)
@@ -75,12 +110,13 @@ public class SMLSerializer extends SweResponseSerializer {
         }
 		
 	}
-	
+
+	// TODO: need to find a way to make sure these are put into the right location
+	// of the SensorML instance
 	public void setDescription(String description){
         try
         {
-            Element systemElt = dom.getElement(smlElt, "sml:SensorML/sml:member/sml:System");
-            dom.setElementValue(smlElt, "gml:description", description);        
+            dom.setElementValue(systemElt, "gml:description", description);        
          }
         catch (Exception e)
         {
@@ -92,13 +128,12 @@ public class SMLSerializer extends SweResponseSerializer {
 	
 	// TODO: need to find a way to make sure these are put into the right location
 	// of the SensorML instance
-	public void setIdentifier(String identifierName, String url, String value){
+	public void addIdentifier(String identifierName, String definitionUrl, String value){
 	       try
 	        {
-	            Element systemElt = dom.getElement(smlElt, "sml:SensorML/sml:member/sml:System");
-	            Element identElt = dom.getElement(systemElt, "sml:identification/IdentifierList/identfier");
+	            Element identElt = dom.addElement(systemElt, "sml:identification/IdentifierList/identfier");
 	        	dom.setAttributeValue(identElt, "@name",identifierName);
-	 	        dom.setAttributeValue(identElt, "sml:Term/@definition", url); 
+	 	        dom.setAttributeValue(identElt, "sml:Term/@definition", definitionUrl); 
 	 	        dom.setElementValue(identElt,"sml:Term/sml:value", value);
 	         }
 	        catch (Exception e)
@@ -108,34 +143,35 @@ public class SMLSerializer extends SweResponseSerializer {
 		
 	}
 	
-	public boolean setIdentifierValue(String identifierName, String value){
+	public void setIdentifierValue(String identifierName, String value){
 	       try
 	        {
-	            Element systemElt = dom.getElement(smlElt, "sml:SensorML/sml:member/sml:System");
 	            Element identElt = dom.getElement(systemElt, "sml:identification/IdentifierList");
 	            NodeList childs = dom.getChildElements(identElt);
 	            for (int i=0; i<childs.getLength(); i++){
 	            	String nameVal = dom.getAttributeValue((Element)childs.item(i),"@name");
+	            	// if exist, set the values in existing elements
 	            	if (nameVal.equalsIgnoreCase(identifierName)){
 	    	 	        dom.setElementValue(identElt,"sml:Term/sml:value", value);
-	            		return true;
 	            	}
+	            	// if does not exist, add elements and set the values as best as possible
+	            	else addIdentifier(identifierName,"",value);
 	            }
 	         }
 	        catch (Exception e)
 	        {
 	            e.printStackTrace();
 	        }
-            return false;		
 	}
-	
-	public void setClassifier(String classifierName, String url, String value, String codespace){
+
+	// TODO: need to find a way to make sure these are put into the right location
+	// of the SensorML instance
+	public void addClassifier(String classifierName, String definitionUrl, String value, String codespace){
 	       try
 	        {
-	            Element systemElt = dom.getElement(smlElt, "sml:SensorML/sml:member/sml:System");
-	            Element classElt = dom.getElement(systemElt, "sml:classification/ClassifierList/classifier");
+	            Element classElt = dom.addElement(systemElt, "sml:classification/ClassifierList/classifier");
 	        	dom.setAttributeValue(classElt, "@name",classifierName);
-	 	        dom.setAttributeValue(classElt, "sml:Term/@definition", url); 
+	 	        dom.setAttributeValue(classElt, "sml:Term/@definition", definitionUrl); 
 	 	        dom.setAttributeValue(classElt, "sml:Term/@codespace", codespace); 
 			 	dom.setElementValue(classElt,"sml:Term/sml:value", value);
 	         }
@@ -146,31 +182,73 @@ public class SMLSerializer extends SweResponseSerializer {
 	}
 
 	
-	public boolean setClassifierValue(String classifierName, String value){
+	public void setClassifierValue(String classifierName, String value){
 	       try
 	        {
-	            Element systemElt = dom.getElement(smlElt, "sml:SensorML/sml:member/sml:System");
 	            Element classElt = dom.getElement(systemElt, "sml:classification/ClassifierList");
 	            NodeList childs = dom.getChildElements(classElt);
 	            for (int i=0; i<childs.getLength(); i++){
 	            	String nameVal = dom.getAttributeValue((Element)childs.item(i),"@name");
+	            	// if exist, set the values in existing elements
 	            	if (nameVal.equalsIgnoreCase(classifierName)){
 	    	 	        dom.setElementValue(classElt,"sml:Term/sml:value", value);
-	            		return true;
 	            	}
+	            	// if does not exist, add elements and set the values as best as possible
+	            	else addClassifier(classifierName,"",value,"");
 	            }
 	         }
 	        catch (Exception e)
 	        {
 	            e.printStackTrace();
 	        }
-         return false;		
 	}
 
-	public void setContactValue(String firstName, String lastName, String street,
-			String city, String state, String zip, String phone, String email){
-		
+	public void addContactValue(String role, String fullName, String organization, String street,
+			String city, String state, String country, String zip, String phone, String email){
+
+        Element contactElt = dom.addElement(systemElt, "sml:contact");
+        if (!role.equalsIgnoreCase(""))
+        	dom.setAttributeValue(contactElt, "@xlink:arcrole","urn:ogc:def:role:OGC::"+role);
+        if (!fullName.equalsIgnoreCase(""))
+        	dom.setElementValue(contactElt,"sml:ResponsibleParty/individualName", fullName);
+        if (!organization.equalsIgnoreCase(""))
+        	dom.setElementValue(contactElt,"sml:ResponsibleParty/organizationName", organization);
+        Element contactInfoElt = dom.addElement(contactElt, "sml:ResponsibleParty/sml:contactInfo");
+        if (!phone.equalsIgnoreCase(""))
+        	dom.setElementValue(contactInfoElt,"phone/voice", phone);
+        if (!street.equalsIgnoreCase(""))
+        	dom.setElementValue(contactInfoElt,"address/deliveryPoint", street);
+        if (!city.equalsIgnoreCase(""))
+        	dom.setElementValue(contactInfoElt,"address/city", city);
+        if (!state.equalsIgnoreCase(""))
+        	dom.setElementValue(contactInfoElt,"address/administrativeArea", state);
+        if (!zip.equalsIgnoreCase(""))
+        	dom.setElementValue(contactInfoElt,"address/postalCode", zip);
+        if (!country.equalsIgnoreCase(""))
+        	dom.setElementValue(contactInfoElt,"address/country", country);
+        if (!email.equalsIgnoreCase(""))
+        	dom.setElementValue(contactInfoElt,"address/electronicMailAddress", email);
 	}
+	
+	/*
+	 public void setLocation(Vector3d location)
+    {
+        try
+        {
+            Element foiElt = dom.addElement(systemElt, "om:featureOfInterest/swe:GeoReferenceableFeature");
+            dom.setElementValue(foiElt, "gml:name", name);
+            
+            Element pointElt = dom.addElement(foiElt, "gml:location/gml:Point");
+            dom.setAttributeValue(pointElt, "@srsName", "urn:ogc:def:crs:EPSG:6.1:4329");
+            
+            dom.setElementValue(pointElt, "gml:coordinates", location.x + " " + location.y + " " + location.z);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }*/
+
 
 }
 
